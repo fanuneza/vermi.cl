2ef83991f9813565b6da03740cc0e10140e0afb4
Fabián <fanuneza@gmail.com>
2026-06-26 00:16:22 -0400

    Integrate Tailwind CSS into Astro configuration and update components for improved styling. Added Tailwind CSS dependencies in package.json and package-lock.json. Refactored BlogCard, Footer, Header, and IsItCompostable components to align with new design principles, enhancing responsiveness and visual consistency. Updated layouts to utilize new styles and improved structure for better user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   adapter: cloudflare({
     mode: 'directory',
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
