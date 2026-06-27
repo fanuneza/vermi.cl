b3faa5b4d129435dd148324fe35ebde0ecdece0b
Fabián <fanuneza@gmail.com>
2026-06-27 18:09:04 -0400

    Enhance site configuration and dependencies by adding new font sources for improved typography. Update astro.config.mjs to always inline stylesheets and adjust Vite build settings. Optimize image loading in the homepage for better performance and user experience. Update Content Security Policy in headers for compliance and security.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://vermi.cl',
   output: 'static',
-
+  build: {
+    inlineStylesheets: 'always',
+  },
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 0,
+    }
   },
 });
